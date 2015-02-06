d50a0a8ef4669582de8694141010d6bc3cb13f1c
XZSanta <XZSanta@gmail.com>
2015-02-06 22:28:28 +0100

    Ready indication
    Flashing ready indication + stand by message

--- IPSC-Timer/IPSC-Timer.ino
+++ IPSC-Timer/IPSC-Timer.ino
@@ -27,14 +27,15 @@ MenuItem mu1_mi1("On (3 sec.)");
 MenuItem mu1_mi2("Off");
 MenuItem mu1_mi3("..");
 Menu mu2("Echo-Cancel");
-MenuItem mu2_mi1("10 ms");
-MenuItem mu2_mi2("20 ms");
-MenuItem mu2_mi3("..");
+MenuItem mu2_mi1("+10 ms");
+MenuItem mu2_mi2("-10 ms");
+MenuItem mu2_mi3("Calibrate");
+MenuItem mu2_mi4("..");
 
 // this constant won't change:
 const int DetectorPin = 2;
-const int BuzzerPin = 8; //
-//const int BuzzerPin = 13; //Quiet night-mode for testing wile the family is asleep
+//const int BuzzerPin = 8; //
+const int BuzzerPin = 13; //Quiet night-mode for testing while the family is asleep
 
 // Variables will change:
 int ShotCounter = 0;
@@ -45,6 +46,9 @@ int XPos = 0;
 int DelayedStart = EEPROM.read(1);
 int DelayedStartTime = 3000;
 
+int interval = 700;
+int intervalState = 1;
+
 int State = 0;
 
 // the following variables are long's because the time, measured in miliseconds,
@@ -52,6 +56,10 @@ int State = 0;
 long lastDebounceTime = 0;  // The last time a shot was detected
 long debounceDelay = 10;    //"Deaf-time" in ms after registered shot
 long StartTime = 0;
+
+long currentMillis;
+long previousMillis;
+
 float LatestShotTime = 0;
 float FirstShotTime = 0;
 float PrevShotTime = 0;
@@ -74,7 +82,8 @@ void setup() {
   mm.add_menu(&mu2);
   mu2.add_item(&mu2_mi1, &on_item6_selected);
   mu2.add_item(&mu2_mi2, &on_item7_selected);
-  mu2.add_item(&mu2_mi3, &on_item8_selected); //..
+  mu2.add_item(&mu2_mi3, &on_item8_selected);
+  mu2.add_item(&mu2_mi4, &on_item9_selected); //..
   ms.set_root_menu(&mm);
 
   lcd.begin();
@@ -91,6 +100,18 @@ void loop() {
 
   switch (State) {
     case 0:  //Ready state
+      currentMillis = millis();
+      if (currentMillis - previousMillis > interval) {
+        previousMillis = currentMillis;
+        if (intervalState == LOW){
+          intervalState = HIGH;
+          lcd.clear();
+        }
+        else {
+          intervalState = LOW;
+          DisplayReady();
+        }
+      }
       if (StartButton.wasReleased()) {
         State = 5;
         StartTimer();
@@ -124,7 +145,7 @@ void loop() {
       if (StartButton.pressedFor(LONG_PRESS)) {
         State = 3;
         lcd.clear();
-        DisplayTimer();
+        DisplayReady();
       }
       break;
 
@@ -144,8 +165,11 @@ void loop() {
         State = 3;
         lcd.clear();
         ResetTimer();
-        DisplayTimer();
+        DisplayReady();
       }
+      break;
+    case 6: //Calibrate
+      DetectCalibrateShots();
       break;
 
   } //End of switch-machine
@@ -181,16 +205,30 @@ void DetectShots() {
       }
     }
   }
+}
 
-
+void DetectCalibrateShots() {
+  //  if (DetectorState != LastDetectorState) {
+  //    if (DetectorState == HIGH) {
+  //        lastDebounceTime = (millis();
+  //    }
+  //    if (DetectorState == LOW) {
+  //        debounceDelay = ((millis()-lastDebounceTime)+10);
+  //    }
+  //  }
+  //
 }
 
 void StartTimer() {
   if (DelayedStart == true) {
+    lcd.clear();
+    DisplayStandby();
     delay (DelayedStartTime);
   }
   StartTime = millis();
   Beep();
+  lcd.clear();
+  DisplayTimer();
 }
 
 void ResetTimer() {
@@ -231,27 +269,38 @@ void DisplayTimer() {
   //lcd.setFontSize(FONT_SIZE_MEDIUM);
   lcd.setCursor(XPos, 4);
   lcd.println((LatestShotTime), 2);
-//  lcd.printLong((LatestShotTime)* 100); //Test of number only font
+  //  lcd.printLong((LatestShotTime)* 100); //Test of number only font
 }
 
 void ClearEEPROM() {
   for (int i = 0; i < 512; i++)
     EEPROM.write(i, 0);
-  }
-  
+}
+
 void Beep() {
   digitalWrite(BuzzerPin, HIGH);
   delay(700);
   digitalWrite(BuzzerPin, LOW);
 }
 
+void DisplayReady() {
+  lcd.setCursor(34, 3);
+  lcd.setFontSize(FONT_SIZE_MEDIUM);
+  lcd.print("READY...");
+}
+
+void DisplayStandby() {
+  lcd.setCursor(10, 3);
+  lcd.setFontSize(FONT_SIZE_MEDIUM);
+  lcd.print("Stand By...");
+}
+
 void displayMenu() {
   lcd.setCursor(0, 0);
-  lcd.setFontSize(FONT_SIZE_MEDIUM);
+  lcd.setFontSize(FONT_SIZE_SMALL);
   Menu const* cp_menu = ms.get_current_menu();
 
   lcd.println(cp_menu->get_name());
-
   MenuComponent const* cp_menu_sel = cp_menu->get_selected();
   for (int i = 0; i < cp_menu->get_num_menu_components(); ++i)
   {
@@ -300,6 +349,10 @@ void on_item7_selected(MenuItem * p_menu_item) {
   displayMenu();
 }
 void on_item8_selected(MenuItem * p_menu_item) {
+
+}
+
+void on_item9_selected(MenuItem * p_menu_item) {
   ms.back();
   displayMenu();
 }
